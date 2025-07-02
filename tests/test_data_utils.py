163a42a2abcf3f5fc4edbf76a1d26e4612451856
Daniel Cripps <binary.riviera@gmail.com>
2025-07-02 20:04:31 +0100

    more tests for data_utils

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -1,11 +1,46 @@
 from datetime import datetime, timedelta
 import pandas as pd
-from tiny_backtester.data_utils import is_regularly_spaced, resample_market_data
+import pytest
+from tiny_backtester.backtester_exception import BacktesterException
+from tiny_backtester.data_utils import (
+    is_regularly_spaced,
+    load_timeseries,
+    resample_market_data,
+)
 
 
 def get_timeseries_dataframe(interval: str = "min"):
     index = pd.date_range("1/1/2000", periods=10, freq=interval)
     return pd.DataFrame({"col1": range(10)}, index=index)
+
+
+def test_load_timeseries_dataframe_valid():
+    valid_df = pd.DataFrame(
+        {
+            "datetime": pd.date_range("1/1/2000", periods=5, freq="min"),
+            "open": [1, 2, 3, 4, 5],
+            "high": [1, 2, 3, 4, 5],
+            "low": [1, 2, 3, 4, 5],
+            "close": [1, 2, 3, 4, 5],
+        }
+    )
+    (ticker, _) = load_timeseries(valid_df, "TEST")
+    assert ticker == "TEST"
+
+
+def test_load_timeseries_dataframe_missing_cols():
+    invalid_df = pd.DataFrame(
+        {
+            "datetime": pd.date_range("1/1/2000", periods=5, freq="min"),
+            "wrong": [1, 2, 3, 4, 5],
+        }
+    )
+    with pytest.raises(BacktesterException, match="missing columns:*"):
+        load_timeseries(invalid_df, "TEST")
+
+
+def test_load_timeseries_is_irregular():
+    pass
 
 
 def test_is_regularly_spaced_true():
@@ -43,3 +78,10 @@ def test_resample_market_data_downsample():
     assert len(res["1"]) == 5
     assert len(res["2"]) == 10
     assert pd.Timedelta(res["1"].index[1] - res["1"].index[0]) == pd.Timedelta("2d")
+
+
+def test_resample_market_data_invalid_resample():
+    df1 = get_timeseries_dataframe("1d")
+    df2 = get_timeseries_dataframe("2d")
+    with pytest.raises(BacktesterException, match="unsupported sampling type"):
+        resample_market_data({"1": df1, "2": df2}, "foobar")  # type: ignore
