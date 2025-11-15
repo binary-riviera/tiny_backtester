18514d8bedd608a8dd4ea3852e8ecbc13bd970a6
Daniel Cripps <binary.riviera@gmail.com>
2025-11-15 14:53:40 +0000

    add pandera and models for later dataframe validation

--- src/tiny_backtester/utils/backtester_types.py
+++ src/tiny_backtester/utils/backtester_types.py
@@ -1,11 +1,13 @@
 from typing import Literal, NamedTuple, TypedDict
-from pandas import DataFrame, Timestamp
 from numpy import float64
+from functools import partial
+import pandas as pd
+import pandera.pandas as pa
 
 OrderType = Literal["buy", "sell"]
 OrderStatus = Literal["filled", "rejected", "unsupported"]
-MarketData = dict[str, DataFrame]
-RunResults = TypedDict("RunResults", {"orders": DataFrame, "positions": dict[str, DataFrame]})
+MarketData = dict[str, pd.DataFrame]
+RunResults = TypedDict("RunResults", {"orders": pd.DataFrame, "positions": dict[str, pd.DataFrame]})
 
 
 class Order(NamedTuple):
@@ -16,7 +18,7 @@ class Order(NamedTuple):
 
 
 class ExecutedOrder(NamedTuple):  # TODO: add id
-    time: Timestamp
+    time: pd.Timestamp
     ticker: str
     type: OrderType
     quantity: int
@@ -25,9 +27,24 @@ class ExecutedOrder(NamedTuple):  # TODO: add id
 
 
 class Position(NamedTuple):
-    time: Timestamp = Timestamp.min
+    time: pd.Timestamp = pd.Timestamp.min
     quantity: int = 0
     entry_price: float64 = float64(0)
     fill_price: float64 = float64(0)
     unrealised_pnl: float64 = float64(0)
     realised_pnl: float64 = float64(0)
+
+
+class Dataset(pa.DataFrameModel):
+    idx: pd.DatetimeIndex
+    open: pd.Series[float] = pa.Field(gt=0, coerce=True)
+    high: pd.Series[float] = pa.Field(gt=0, coerce=True)
+    low: pd.Series[float] = pa.Field(gt=0, coerce=True)
+    close: pd.Series[float] = pa.Field(gt=0, coerce=True)
+    volume: pd.Series[int] = pa.Field(gt=0, coerce=True)
+
+
+class PrecalcDataset(Dataset):
+    midpoint: pd.Series[float] = pa.Field(gt=0, coerce=True)
+    slippage: pd.Series[float] = pa.Field(gt=0, coerce=True)
+    spread: pd.Series[float] = pa.Field(gt=0, coerce=True)
